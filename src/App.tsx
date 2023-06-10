817a45058482b09e6950a07ca76afbd89744a1a2
Thuan Nguyen <nguyenquangthuan2906@gmail.com>
2023-06-10 12:08:10 +0700

    add

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React1231231asdasdasdsd
+          Learn React1231231asdasdasdsd test
         </a>
       </header>
     </div>
